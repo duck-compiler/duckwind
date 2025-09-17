b17f832dea089c61901eb55615dc4b50482520d1
Robin Füllgraf <robin@fuellgraf.dev>
2025-09-18 01:35:34 +0200

    feat: watch flag

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
 use duckwind::EmitEnv;
 
+use notify::{Event, RecursiveMode, Result, Watcher};
+use std::{path::Path, sync::mpsc, time::Instant};
+
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(name = "input", help = "load the input from this file")]
-    in_file: String,
+    in_file: Vec<String>,
     #[arg(
         long = "str",
         short = 's',
@@ -24,42 +27,78 @@ struct Args {
     no_default_config: bool,
     #[arg(long, short = 'c', help = "load this config")]
     config: Vec<String>,
+    #[arg(
+        long,
+        short = 'w',
+        help = "watch a file, relaunching with the same parameters if it changes (requires out file)"
+    )]
+    watch: Option<String>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Args::parse();
 
-    let mut emit_env = if cli.no_default_config {
-        EmitEnv::new()
-    } else {
-        EmitEnv::new_with_default_config()
-    };
+    let run = || {
+        let mut emit_env = if cli.no_default_config {
+            EmitEnv::new()
+        } else {
+            EmitEnv::new_with_default_config()
+        };
 
-    for config_to_load in &cli.config {
-        let config_src = std::fs::read_to_string(config_to_load.as_str())
-            .expect(&format!("couldn't load config {config_to_load}"));
-        emit_env.load_config(&config_src);
-    }
-
-    let file_name = std::env::args().nth(1).expect("no file name provided");
-    let txt = if cli.from_string {
-        cli.in_file
-    } else {
-        std::fs::read_to_string(&file_name).expect("Could not read input file")
-    };
-    let mut i = 0;
-    while i < txt.len() {
-        if let Some((_, skip)) = emit_env.parse_tailwind_str(&txt[i..]) {
-            i += skip;
+        for config_to_load in &cli.config {
+            let config_src = std::fs::read_to_string(config_to_load.as_str())
+                .expect(&format!("couldn't load config {config_to_load}"));
+            emit_env.load_config(&config_src);
         }
-        i += 1;
-    }
 
-    let as_css = emit_env.to_css_stylesheet(!cli.no_preflight);
+        let txt = if cli.from_string {
+            cli.in_file.clone()
+        } else {
+            cli.in_file
+                .iter()
+                .map(|file_name| {
+                    std::fs::read_to_string(file_name).expect("Could not read input file")
+                })
+                .collect()
+        };
 
-    if let Some(out) = cli.out {
-        std::fs::write(out, as_css).expect("Could not write output file");
+        for txt in txt {
+            let mut i = 0;
+            while i < txt.len() {
+                if let Some((_, skip)) = emit_env.parse_tailwind_str(&txt[i..]) {
+                    i += skip;
+                }
+                i += 1;
+            }
+        }
+
+        let as_css = emit_env.to_css_stylesheet(!cli.no_preflight);
+
+        if let Some(out) = cli.out.as_ref() {
+            std::fs::write(out, as_css).expect("Could not write output file");
+        } else {
+            println!("{as_css}");
+        }
+    };
+
+    if let Some(watch) = cli.watch.as_ref() {
+        if cli.out.is_none() {
+            println!("error: watch requires out file");
+            return Ok(());
+        }
+        let (tx, rx) = mpsc::channel::<Result<Event>>();
+        let mut watcher = notify::recommended_watcher(tx)?;
+        watcher.watch(Path::new(watch.as_str()), RecursiveMode::Recursive)?;
+        run();
+        println!("Watching... (Ctrl+C to exit)");
+        for _ in rx {
+            let inst = Instant::now();
+            run();
+            println!("Recompiled in {}ms.", inst.elapsed().as_millis());
+        }
     } else {
-        println!("{as_css}");
+        run();
     }
+
+    Ok(())
 }
